97e1e4e5e2000c31c0756590e80fdc8dfc10037d
lidagorvat <lidgorvat@gmail.com>
2020-08-09 12:11:10 -0300

    ej 7

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,69 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	//Declaro las variables y las inicializo
+	let num1=parseInt(document.getElementById("txtIdNumeroUno").value);
+	let num2=parseInt(document.getElementById("txtIdNumeroDos").value);
+	let resultado;
+
+	//Realizo el cálculo
+	resultado = num1 + num2;
+
+	//Muestro el resultado por alert
+	alert("El resultado de la suma es: "+resultado);	
 }
 
 function restar()
 {
-	
+	//Declaro las variables y las inicializo
+	let num1=parseInt(document.getElementById("txtIdNumeroUno").value);
+	let num2=parseInt(document.getElementById("txtIdNumeroDos").value);
+	let resultado;
+
+	//Realizo el cálculo
+	resultado=num1-num2;
+
+	//Muestro el resultado por alert
+	alert("el resultado de la resta es: "+resultado); 
 }
 
 function multiplicar()
 { 
-	
+	//Declaro las variables y las inicializo
+	let num1=parseInt(document.getElementById("txtIdNumeroUno").value);
+	let num2=parseInt(document.getElementById("txtIdNumeroDos").value);
+	let resultado;
+
+	//Realizo el cálculo
+	resultado=num1*num2;
+
+	//Muestro el resultado por alert
+	alert("El producto de los numeros es: "+resultado);
 }
 
 function dividir()
 {
-	
-}
+	//Declaro e inicializo las variables
+	let num1=parseInt(document.getElementById("txtIdNumeroUno").value);
+	let num2=parseInt(document.getElementById("txtIdNumeroDos").value);
+	let resultado;
 
+	//Realizo una secuencia lógica para impedir la division por 0
+
+	//Si el divisor es 0 muestra un error por alert
+	if(num2=="0")
+		{
+			
+			alert("La división ingresada no es válida");
+		}
+	
+	//Si el divisor es distinto de 0 el programa realiza el calculo y lo muestra por pantalla
+	else
+	{
+		
+		num1=parseInt(num1);
+		num2=parseInt(num2);
+		resultado = num1 / num2;
+		alert("El resultado de la division es: "+resultado);
+	}
+}
