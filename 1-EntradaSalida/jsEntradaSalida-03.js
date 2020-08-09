85b9264a0df064b4a579f9fe94622e0078691e94
lidagorvat <lidgorvat@gmail.com>
2020-08-09 11:32:54 -0300

    curso de ingreso

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,10 +3,14 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("Juan");
+	//Declaro la variable
+	let nombre;
 
+	//Asigno valor a la variable a traves de ID
+	nombre=document.getElementById("txtIdNombre").value;
+	
+	//Muestro el nombre
+	alert("El nombre ingresado es: " +nombre);
 }
 
 
