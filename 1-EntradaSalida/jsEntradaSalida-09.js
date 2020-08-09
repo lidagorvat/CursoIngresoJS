fc241063a0024c3e4e2ed5bc92189ff359af47d2
lidagorvat <lidgorvat@gmail.com>
2020-08-09 12:13:50 -0300

    ej 9

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,20 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+//Declaro las variables
+let sueldo = document.getElementById("txtIdSueldo").value;
+let aumento;
+let resultado;
 
-	alert("ok");
+//Convierto el sueldo a numero
+sueldo=parseFloat(sueldo);
+
+//Calculo el aumento
+aumento = sueldo * 10 / 100;
+
+//Aplico el incremento
+resultado = sueldo + aumento;
+
+//Muestro el resultado por el cuadro de texto
+document.getElementById("txtIdResultado").value=resultado;
 }
