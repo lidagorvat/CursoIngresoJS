5ddcd9b731ac173825a7345fc650a303965cd4de
lidagorvat <lidgorvat@gmail.com>
2020-08-09 12:14:53 -0300

    ej 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,22 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	//Declaro las variables
+	let importe=document.getElementById("txtIdImporte").value;
+	let descuento;
+	let resultado;
+
+	//Convierto el importe a numero
+	importe=parseFloat(importe);
+
+	//Calculo el descuento
+	descuento = importe * 25 / 100;
+
+	//Calculo el resultado
+
+	resultado = importe - descuento;
+
+	//Muestro el resultado en el cuadro de texto
+	document.getElementById("txtIdResultado").value=resultado;
+
 }
