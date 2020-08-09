2dd40ff23b40cd41a735a05e0912ed893f012b3a
lidagorvat <lidgorvat@gmail.com>
2020-08-09 12:01:19 -0300

    Update jsEntradaSalida-04.js

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -3,8 +3,13 @@ Debemos lograr tomar un dato por 'PROMPT'
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
-{let nombre
-	nombre = "PEPITO"
-	alert(nombre);
-}
+{//Declaro la variable
+	let nombre;
 
+	//Pedir el dato por prompt
+	nombre= prompt("Inserte un nombre");
+
+	//Asigno el dato ingresado al cuadro de texto
+	document.getElementById("txtIdNombre").value = nombre;
+
+}
