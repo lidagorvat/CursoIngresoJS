6af159c5ee15a7c00aefffea1da5f26281d039fe
lidagorvat <lidgorvat@gmail.com>
2020-08-10 12:05:47 -0300

    if 9

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,15 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	//Declaro las variables y las inicializo
+
+	let edad = parseInt(document.getElementById("txtIdEdad").value);
+	let estadoCivil = document.getElementById("estadoCivil").value;
+
+	/*Sentencia lógica: Si la edad es mayor o igual a 18 y el estado es igual a soltero
+	  muestra "Es mayor y esta soltero" por alert*/
 	
-
-
+	  if (edad>=18 && estadoCivil=="Soltero")
+	{
+		alert("Es mayor y esta soltero");
+	}
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,16 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+		//Genero un numero aleatorio con la funcion Math.random() y lo multiplico por 11
+	let aleatorio;
+	let minimo = 1;
+	let maximo = 10;
+
+	//Transformo el numero aleatorio a numero entero (es flotante por defecto)
+	aleatorio = Math.round(Math.random() * (maximo-minimo) + minimo);
+
+	aleatorio = parseInt(aleatorio);
+
+	//Muestro el numero por alert
+	alert("Su numero es "+ Math.ceil(aleatorio));	
 
 }//FIN DE LA FUNCIÓN
