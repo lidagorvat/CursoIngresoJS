c4fd49b6b0c45159b3118844532e72f706c4e5e3
lidagorvat <lidgorvat@gmail.com>
2020-08-10 01:03:38 -0300

    tp10listo

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,26 +5,52 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-    let producto1
-    let producto2
-    let producto3
-    let resultado
+    //Declaro las variables
+    let prod1 = parseFloat(document.getElementById("txtIdPrecioUno").value);
+    let prod2 = parseFloat(document.getElementById("txtIdPrecioDos").value);
+    let prod3 = parseFloat(document.getElementById("txtIdPrecioTres").value);
+    let suma;
 
-    producto1 = parseFloat (document.getElementById("txtIdPrecioUno").value);
-    producto2 = parseFloat (document.getElementById("txtIdPrecioDos").value);
-    producto3 = parseFloat (document.getElementById("txtIdPrecioTres").value);
-    resultado = producto1 + producto2 + producto3;
-
-    prompt = resultado;
-    alert (resultado);
-
-
+    //Calculo la suma
+    suma = prod1+prod2+prod3;
+    suma = suma.toFixed(2);
+    
+    //Muestro el resultado por alert
+    alert("La suma de los precios es $"+suma);
+    
 }
 function Promedio () 
 {
-	
+    //Declaro las variables
+    let prod1 = parseFloat(document.getElementById("txtIdPrecioUno").value);
+    let prod2 = parseFloat(document.getElementById("txtIdPrecioDos").value);
+    let prod3 = parseFloat(document.getElementById("txtIdPrecioTres").value);
+    let promedio;
+    
+    //Calculo el promedio
+    promedio = (prod1+prod2+prod3) / 3;
+    promedio = promedio.toFixed(2);
+
+    //Muestro el promedio por alert
+    alert("El promedio de los productos es $" +promedio);
 }
 function PrecioFinal () 
 {
-	
+     //Declaro las variables
+     let prod1 = parseFloat(document.getElementById("txtIdPrecioUno").value);
+     let prod2 = parseFloat(document.getElementById("txtIdPrecioDos").value);
+     let prod3 = parseFloat(document.getElementById("txtIdPrecioTres").value);
+     let suma;
+     let iva;
+     let precioFinal;
+
+     //Calculo el valor del precio final (suma de productos, calculo de IVA y suma total)
+     suma = (prod1+prod2+prod3)
+     iva = suma * 21 / 100;
+     precioFinal = suma + iva;
+     precioFinal = precioFinal.toFixed(2)
+
+     //Muestro el precio final por alert
+     alert("El precio final es $"+precioFinal);
+     
 }
