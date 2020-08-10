6a0ce530ab04a71043d6c6a81089bc2e5818a5f7
lidagorvat <lidgorvat@gmail.com>
2020-08-10 12:00:43 -0300

    if

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,25 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+		//Declaro las variables y tomo la edad y la convierto a numero
+		let edad=document.getElementById("txtIdEdad").value;
 
-
+		edad=parseInt(edad);
+	
+		/*Secuencia lógica: Si la edad es mayoro igual a 18, muestra por alert "Es mayor de edad".Si la edad es menor
+		a 18 pero mayor o igual a 13, muestra por alert "Es adolescente". Sino, muestra por alert "Es un niño*/
+		if(edad>=18)
+		{
+			alert("Es mayor de edad");
+		}
+	
+		else if(edad>=13 && edad<=17)
+		{
+			alert("Es adolescente");
+		}
+	
+		else
+		{
+			alert("Es un niño");
+		}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,17 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	//Declaro la variable
+	let edad;
+	let estadoCivil;
 	
-
-
+	//Inicializo las variables y las convierto a numero / boolean
+	edad=document.getElementById("txtIdEdad").value;
+	edad = parseInt(edad);
+	estadoCivil = document.getElementById("estadoCivil").value;
+	
+	//Secuencia lógica: Si la edad es menor a 18 y el estado civil es distinto de soltero muestra por alert el mensaje 
+	if(edad<18 && estadoCivil!="Soltero")
+	{
+		alert("Es muy pequeño para NO ser soltero");
+	}
 }//FIN DE LA FUNCIÓN
