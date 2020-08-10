f072e4084fce570de37ccfb9ee91b3c12826cc0c
lidagorvat <lidgorvat@gmail.com>
2020-08-10 12:43:44 -0300

    tp4

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -10,5 +10,79 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+        //Declaro las variables
+        let cantidad = parseInt(document.getElementById("txtIdCantidad").value);
+        let marca = document.getElementById("Marca").value;
+        let descuento;
+        let precioFinal;
+        let precioUnitario;
+        let impuesto;
+        const PRECIO_LAMPARA = 35;
+    
+        switch(cantidad)
+        {
+            case 1:
+            case 2:
+                descuento = 1
+                break;
+            case 3:
+                if (marca == "ArgentinaLuz")
+                {
+                    descuento = 0.85;
+    
+                }
+                else if (marca == "FelipeLamparas")
+                {
+                    descuento = 0.9;
+                }
+                else
+                {
+                    descuento = 0.95;
+                }
+                break;
+            case 4:
+                if (marca == "ArgentinaLuz" || marca == "FelipeLamparas")
+                {
+                    descuento = 0.75;
+                }
+                else
+                {
+                    descuento = 0.8;
+                }
+                break;
+            case 5:
+                if (marca == "ArgentinaLuz")
+                {
+                    descuento = 0.6
+                }
+                else
+                {
+                    descuento = 0.7;
+                }
+                break;
+            default:
+            descuento = 0.5
+            break;
+        }
+    
+        precioUnitario = PRECIO_LAMPARA * descuento;
+        precioFinal = precioUnitario * cantidad;
+    
+        if (precioFinal > 120)
+        {
+            impuesto = precioFinal / 10;
+            precioFinal += impuesto;
+    
+            document.getElementById("txtIdprecioDescuento").value = "$" + precioUnitario.toFixed(2);
+            alert("El monto total es de $"+precioFinal.toFixed(2)+". IIBB: Usted abonó $"+impuesto.toFixed(2)+" en concepto de ingresos brutos");
+        }
+        else
+        {
+            document.getElementById("txtIdprecioDescuento").value = "$" + precioUnitario.toFixed(2);
+            alert("El monto total es de $"+precioFinal.toFixed(2));  
+        }
+    
+    
+
+    
 }
