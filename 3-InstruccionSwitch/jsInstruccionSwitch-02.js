1eafd2cb8e7768387fa91e6b5760ce996a2e0785
lidagorvat <lidgorvat@gmail.com>
2020-08-10 13:26:43 -0300

    2

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,9 +1,36 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes = document.getElementById("txtIdMes").value;
+	
 
+	//Agrupo y muestro mensajes correspondientes
+	switch (mes)
+	{
+		case "Enero":
+		case "Febrero":
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "Junio":
+			alert("Falta para el invierno");
+			break;
+
+		case "Julio":
+		case "Agosto":
+			alert("Abrigate que hace Frio");
+			break;
+
+		case "Septiempre":
+		case "Octubre":
+		case "Noviembre":
+		case "Diciembre":
+		{
+			alert("Ya pasamos el frio, ahora calor!");
+			break;
+		}
+
+	}
 
 
 
