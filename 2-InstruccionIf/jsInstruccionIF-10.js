646e0391f4f8ed1ac5a7a0105129db54cb76770e
lidagorvat <lidgorvat@gmail.com>
2020-08-10 12:30:45 -0300

    10

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,24 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+		//Genero el número RANDOM entre 1 y 10 
+		let nota;
+		let maximo = 10;
+		let minimo = 1;
+	
+		nota = Math.round(Math.random() * (maximo-minimo) + minimo);
+	
+		if(nota>=9)
+		{
+			alert("Su nota es "+nota +". Excelente");
+		}
+	
+		else if(nota>=4 && nota<=8)
+		{
+			alert("Su nota es "+nota +". Esta Aprobado")
+		}
+		else
+		{
+			alert("Su nota es "+ nota + ". Vamos que la proxima se puede")
+		}
 
 }//FIN DE LA FUNCIÓN
