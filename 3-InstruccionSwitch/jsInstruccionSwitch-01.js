2b6c4e4c124c3217f3a9218a2b97c9832ff25d25
lidagorvat <lidgorvat@gmail.com>
2020-08-10 13:23:43 -0300

    verano

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,26 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes = document.getElementById("txtIdMes").value;
+	
+	// Creo el switch y agrego los cuadros de texto
+	switch (mes)
+	{
+	case "Enero":
+			alert("Que comiences bien el año!");
+			break;
+	
+	case "Marzo":
+		alert("Arrancan las clases");
+		break;
 
+	case "Julio":
+			alert("Se vienen las vacaciones!");
+			break;
 
+	case "Diciembre":
+			alert("Felices fiestas!");
+			break;
 
+	}
 }//FIN DE LA FUNCIÓN
